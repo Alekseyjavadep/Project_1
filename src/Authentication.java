8941fb04fb3385c4977651772614f16d329b6896
Alekseyjavadep <timofeevaleksej158@gmail.com>
2021-05-12 21:25:56 +0300

    1

--- src/Authentication.java
+++ src/Authentication.java
@@ -3,14 +3,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Authentication {
-    String password;
-    String email;
-    String login;
-    int counter_password = 0;
-
-    void check_password() {
-        Scanner sc_1 = new Scanner(System.in);
-        password = sc_1.next();
+    void check_password(String password) {
         Pattern pattern = Pattern.compile("^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$");
         Matcher matcher = pattern.matcher(password);
         boolean isPassword = matcher.matches();
@@ -18,22 +11,21 @@ public class Authentication {
             return;
         }
         else System.out.println("Ошибка, введите снова");
-        check_password();
+        //check_password(password);
+        Scanner sc = new Scanner(System.in);
+        String password1 = sc.nextLine();
+        check_password(password1);
     }
-    void check_email() {
-        Scanner sc_2 = new Scanner(System.in);
-        email = sc_2.next();
+    void check_email(String email) {
         Pattern pattern = Pattern.compile("^[\\w-_.]*[\\w-_.]@[\\w].+[\\w]+[\\w]$");
         Matcher matcher = pattern.matcher(email);
         boolean isEmail = matcher.matches();
         if (isEmail == true){
             return;
         } else System.out.println("Ошибка, введите снова");
-        check_email();
+        check_email(email);
     }
-    void check_login() {
-        Scanner sc_2 = new Scanner(System.in);
-        login = sc_2.next();
+    void check_login(String login) {
         Pattern pattern = Pattern.compile("^[a-z]+([-_]?[a-z0-9]+){0,2}$");
         Matcher matcher = pattern.matcher(login);
         boolean isLogin = matcher.matches();
@@ -41,7 +33,7 @@ public class Authentication {
             System.out.println("Вы успешно зарегистрировались!");
             return;
         } else System.out.println("Ошибка, введите снова");
-        check_login();
+        check_login(login);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 import javax.swing.plaf.synth.SynthTabbedPaneUI;
 import java.lang.annotation.ElementType;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,29 +10,46 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Authentication authentication = new Authentication();
-        String login = "login1";
-        String password = "123@pP";
+        Scanner sc = new Scanner(System.in);
+        String password = null;
+        String email;
+        String login = null;
         System.out.println("1 - вход");
         System.out.println("2 = регистрация");
-        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
         String start = sc.nextLine();
-        if (start.equals("1")){
-            System.out.println("Введите ваш логин");
+        if (start.equals("1")) {
+            System.out.println("Введите ниже ваш логин");
             String inputLogin = sc.nextLine();
-            System.out.println("Введите ваш пароль");
             String inputPassword = sc.nextLine();
-            if (inputLogin.equals("login1")){
-                if (inputPassword.equals("123@pP")){
+            if (inputLogin.equals("login1")) {
+                if (inputPassword.equals("123@pP")) {
                     System.out.println("Вы успешно вошли");
                 }
-            }else System.out.println("Ошибка входа");
-        }else{
-            System.out.println("Введите пароль");
-            authentication.check_password();
+            } else System.out.println("Ошибка входа");
+        } else {
+            System.out.println("Введите ваш пароль" + " " + "Пароль должен соотвествовать следующим критериям:");
+            System.out.println("Длинна не меньше 6 символов");
+            System.out.println("Заглавные и прописные буквы");
+            System.out.println("Один из специальных символов:@,#,$,%");
+            password = sc.nextLine();
+            authentication.check_password(password);
             System.out.println("Введите Email");
-            authentication.check_email();
+            email = sc.nextLine();
+            authentication.check_email(email);
             System.out.println("Введите логин");
-            authentication.check_login();
+            login = sc.nextLine();
+            authentication.check_login(login);
+        }
+        System.out.println("Для входа введите ваш логин и пароль");
+        System.out.println("Логин:");
+        String login_input = sc.nextLine();
+        System.out.println("Пароль:");
+        String password_input = sc.nextLine();
+        if (login_input.equals(login)) {
+           if(password_input.equals(password)){
+               System.out.println("Вы успешно вошли");
+           }else System.out.println("Ошибка");
         }
     }
 }
